d2273f958632bed1a9041e62af8a042eee949878
jamesward-99 <jamesward_99@outlook.com>
2021-10-14 14:42:42 +0100

    Project clean up

--- CT417AssignmentOneDriverProject/src/main/java/CT417AssignmentOneDriverProject/DriverClass.java
+++ CT417AssignmentOneDriverProject/src/main/java/CT417AssignmentOneDriverProject/DriverClass.java
@@ -4,7 +4,7 @@ public class DriverClass {
 
 	public static void main(String[] args) {
 		//Student student = new Student("James",22 , "06/08/1999", 18394566);
-		System.out.println("Hi");
+		//System.out.println("Hi");
 	}
 
 }
